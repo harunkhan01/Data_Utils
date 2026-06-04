14b281c501e86cfe8250c0bdd33049f3ce70337a
Harun Khan <harunage@gmail.com>
2026-06-03 20:43:13 -0500

    Basic vector

--- build/CMakeFiles/data.dir/main.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/data.dir/main.c.o: \
- /home/swanfinder/c_projects/Data_Utils/main.c /usr/include/stdc-predef.h \
- /home/swanfinder/c_projects/Data_Utils/include/vector.h \
- /usr/lib/gcc/aarch64-linux-gnu/11/include/stddef.h
